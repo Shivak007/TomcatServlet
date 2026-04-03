d6eb8436cc3941ab0258dc893681c71de609aaaa
Shivak007 <kshivaoffl2824@gmail.com>
2026-04-03 18:45:36 +0530

    [Shiva Kumar] : feat: UC2-Implement login with predefined user

--- src/main/java/com/bridgelabz/LoginServlet.java
+++ src/main/java/com/bridgelabz/LoginServlet.java
@@ -1,4 +1,38 @@
 package com.bridgelabz;
 
-public class LoginServlet {
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        urlPatterns = "/LoginServlet",
+        initParams = {
+                @WebInitParam(name = "user", value = "Sriram"),
+                @WebInitParam(name = "password", value = "BridgeLabz")
+        }
+)
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+
+        String dbUser = getServletConfig().getInitParameter("user");
+        String dbPwd = getServletConfig().getInitParameter("password");
+
+        if (dbUser.equals(user) && dbPwd.equals(pwd)) {
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("LoginSuccess.jsp")
+                    .forward(request, response);
+        } else {
+            PrintWriter out = response.getWriter();
+            out.println("<h3 style='color:red'>Invalid Credentials</h3>");
+            request.getRequestDispatcher("login.html")
+                    .include(request, response);
+        }
+    }
 }
