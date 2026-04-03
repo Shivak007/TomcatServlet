8634d960bb74bd7ffdfb0b19e1c7e1c8e9864e7b
Shivak007 <kshivaoffl2824@gmail.com>
2026-04-03 18:40:47 +0530

    [Shiva Kumar] : feat: UC1-Create first servlet

--- src/main/java/com/bridgelabz/FirstServlet.java
+++ src/main/java/com/bridgelabz/FirstServlet.java
@@ -1,4 +1,18 @@
 package com.bridgelabz;
 
-public class FirstServlet {
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class FirstServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from Servlet</h1>");
+    }
 }
